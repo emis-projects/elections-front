5ebe1fd8afd20393210145cfb993aa07adbccdd2
TataKharatishvili <tata.kharatishvili@gmail.com>
2022-09-06 16:21:03 +0400

    minor changes

--- javascript/burger-menu.js
+++ javascript/burger-menu.js
@@ -3,8 +3,20 @@ const showNav = document.querySelectorAll(".burger-menu-navbar");
 
 burger.forEach(singleBurger => {
   singleBurger.addEventListener("click", () => {
+    console.log(1);
     showNav.forEach(singleShowNav => {
       singleShowNav.classList.toggle("show-burger-menu");
+      console.log(2);
     });
   });
 });
+
+// function hideMenuQuery(x) {
+//   if (!x.matches) {
+//     showNav.style.display = "none";
+//   }
+// }
+
+// let x = window.matchMedia("(max-width: 768px)");
+// hideMenuQuery(x);
+// x.addListener(hideMenuQuery);
